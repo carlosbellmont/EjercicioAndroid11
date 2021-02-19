e7668f48c1b895825c6f9be66ff05ea2cb341e4e
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2021-02-19 23:45:03 +0100

    Initial commit

---
+++ app/src/main/java/com/cbellmont/ejercicioandroid11/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.cbellmont.ejercicioandroid11
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.cbellmont.ejercicioandroid11.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var adapter : PersonajesAdapter
+    private val listaPersonajes = loadData()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        createRecyclerView()
+
+        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                binding.rbTodos.id -> adapter.updatePersonajes(listaPersonajes)
+                binding.rbBuenos.id -> adapter.updatePersonajes(listaPersonajes.filter { it.esBueno })
+                binding.rbMalos.id -> adapter.updatePersonajes(listaPersonajes.filter { !it.esBueno })
+            }
+        }
+    }
+
+    private fun createRecyclerView() {
+        adapter = PersonajesAdapter(listaPersonajes)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+    }
+
+
+    private fun loadData() : List<Personaje>{
+        val personaje1 = Personaje("Aragorn", "Humano", R.mipmap.aragorn, true)
+        val personaje2 = Personaje("Gandalf", "Mago", R.mipmap.gandalf, true)
+        val personaje3 = Personaje("Boromir", "Humano", R.mipmap.boromir, true)
+        val personaje4 = Personaje("Legolas", "Elfo", R.mipmap.legolas, true)
+        val personaje5 = Personaje("Orco Feo", "Orco", R.mipmap.orco, false)
+        val personaje6 = Personaje("Smagu", "Dragon", R.mipmap.smagu, false)
+
+        return mutableListOf(personaje1,personaje2,personaje3,personaje4,personaje5,personaje6).apply { shuffle() }
+    }
+}
---
+++ app/src/main/java/com/cbellmont/ejercicioandroid11/PersonajesAdapter.kt
@@ -0,0 +1,43 @@
+package com.cbellmont.ejercicioandroid11
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class PersonajesAdapter(var listaPersonajes : List<Personaje>) : RecyclerView.Adapter<PersonajesAdapter.PersonajesViewHolder>()  {
+
+    class PersonajesViewHolder(val root: View, val  imageView: ImageView, val textViewNombre: TextView, val textViewRaza : TextView) : RecyclerView.ViewHolder(root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonajesViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+        val ivFoto = view.findViewById<ImageView>(R.id.iwFoto)
+        val tvRaza = view.findViewById<TextView>(R.id.tvRaza)
+        val tvNombre = view.findViewById<TextView>(R.id.tvNombre)
+
+        return PersonajesViewHolder(view, ivFoto, tvNombre, tvRaza)
+    }
+
+    override fun getItemCount(): Int {
+        return listaPersonajes.size
+    }
+
+    override fun onBindViewHolder(holder: PersonajesViewHolder, position: Int) {
+        holder.imageView.setImageResource(listaPersonajes[position].image)
+        holder.textViewNombre.text = listaPersonajes[position].nombre
+        holder.textViewRaza.text = listaPersonajes[position].raza
+        holder.root.setBackgroundColor(ContextCompat.getColor(holder.root.context, if (listaPersonajes[position].esBueno) R.color.colorBgBueno else R.color.colorBgMalo))
+
+        holder.imageView.setOnClickListener{ Toast.makeText(it.context, "Yo me llamo ${listaPersonajes[position].nombre}", Toast.LENGTH_LONG).show() }
+    }
+
+    fun updatePersonajes(newData: List<Personaje>){
+        listaPersonajes = newData
+        notifyDataSetChanged()
+    }
+}
+
